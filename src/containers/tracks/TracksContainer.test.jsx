8a49c8a1a08dce9c30b15b2c37ead114233e5f85
Cherno Badjan <badjancherno@gmail.com>
2021-05-27 11:28:50 -0700

    trackslist container test passing

--- src/containers/tracks/TracksContainer.test.jsx
+++ src/containers/tracks/TracksContainer.test.jsx
@@ -1,12 +1,34 @@
+require('dotenv').config();
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { MemoryRouter } from 'react-router-dom';
 import TracksContainer from './TracksContainer';
+import tracksContainer from '../../apiData/tracksContainer.json';
+
+const server = setupServer(
+  rest.get(
+    // eslint-disable-next-line max-len
+    'http://musicbrainz.org/ws/2/recording',
+    (req, res, ctx) => {
+      return res(ctx.json(tracksContainer));
+    }
+  )
+);
 
 describe('TracksContainer', () => {
-  it('displays a list of albums', async () => {
-    render(<TracksContainer />);
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+  it('displays a list of tracks', async () => {
+    render(
+      <MemoryRouter>
+        <TracksContainer />
+      </MemoryRouter>
+    );
 
-    screen.getByText('Loading...');
-    // const ulEl = screen.getByLabelText('albums');
+    screen.findByAltText('spinner');
+    const ulEl = await screen.findByRole('list', { name: 'track-list' });
+    expect(ulEl).not.toBeEmptyDOMElement();
   });
 });
